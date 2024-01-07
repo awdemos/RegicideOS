30fd26f946392a9387c7892333c6de06bd038b3a
Luna Deards <luna@xenialinux.com>
2024-01-07 06:08:43 +0000

    Resolve "Add manifest logic to installer"

--- config.py
+++ config.py
@@ -1,4 +1,39 @@
 import common
+import tomllib
+import urllib.request
+import subprocess
+
+# The following stuff is verbatim copied from foxupdate. I'll find a better way later.
+
+ARCH_TABLE = {
+    "x86_64": "amd64"
+}
+
+arch = ARCH_TABLE[subprocess.run(["arch"], shell=True, capture_output=True, text=True).stdout.strip()]
+
+def get_flavours(ret):
+    manifest = tomllib.load(urllib.request.urlopen(f"{ret['repository']}Manifest.toml"))
+    return [flavour for flavour in manifest.keys() if arch in manifest[flavour]["arch"]]
+
+
+def get_releases(ret):
+    manifest = tomllib.load(urllib.request.urlopen(f"{ret['repository']}Manifest.toml"))
+    return [release for release in manifest[ret["flavour"]]["versions"] if arch in manifest[ret["flavour"]]["versions"][release]["arch"]]
+
+    
+def get_url(config_parsed):
+    manifest = tomllib.load(
+        urllib.request.urlopen(f"{config_parsed['repository']}Manifest.toml")
+    )
+
+    filename = manifest[config_parsed["flavour"]]["versions"][
+        config_parsed["release_branch"]
+    ]["filename"]
+
+    return f"{config_parsed['repository']}{arch}/{config_parsed['release_branch']}/{filename}"
+
+
+# End of foxupdate code
 
 
 def fix_config(
@@ -38,11 +73,23 @@ def parse_config(config_file: dict, interactive: bool = False) -> dict or str:
 
     VALIDITY: dict = {
         "drive": {"func": common.get_drives, "mode": "execute", "default": ""},
-        "root_url": {
+        "repository": {
             "func": common.check_url,
             "mode": "check",
-            "default": "https://repo.xenialinux.com/releases/current/root-systemd.img",
-            "valid_text": "a URL that points to a root.img",
+            "default": "https://repo.xenialinux.com/releases/",
+            "valid_text": "a URL that points to a Xenia repository (remember the trailing slash)",
+        },
+        "flavour": {
+            "func": get_flavours,
+            "mode": "execute",
+            "default": "gnome-systemd",
+            "return": True,
+        },
+        "release_branch": {
+            "func": get_releases,
+            "mode": "execute",
+            "default": "main",
+            "return": True,
         },
         "filesystem": {"func": common.get_fs, "mode": "execute", "default": "btrfs"},
         "username": {
@@ -63,7 +110,13 @@ def parse_config(config_file: dict, interactive: bool = False) -> dict or str:
 
         match VALIDITY[key]["mode"]:
             case "execute":
-                valid = VALIDITY[key]["func"]()
+                if "return" in VALIDITY[key].keys():
+                    if VALIDITY[key]["return"] == True:
+                        valid = VALIDITY[key]["func"](ret=config_file) 
+                    else:
+                        valid = VALIDITY[key]["func"]()
+                else:
+                    valid = VALIDITY[key]["func"]()
 
                 if key not in config_file or config_file[key] not in valid:
                     config_file = fix_config(
@@ -71,15 +124,32 @@ def parse_config(config_file: dict, interactive: bool = False) -> dict or str:
                     )
 
             case "check":
-                if key not in config_file or not VALIDITY[key]["func"](
-                    value=config_file[key]
-                ):
-                    config_file = fix_config(
-                        config_file,
-                        interactive,
-                        key,
-                        VALIDITY[key]["valid_text"],
-                        default,
-                    )
+                do_return = False
+                if "return" in VALIDITY[key].keys():
+                    do_return = VALIDITY[key]["return"]
+
+                if do_return: # yes, i know this is horrific. I'm sorry.
+                    if key not in config_file or not VALIDITY[key]["func"](
+                        value=config_file[key], ret=config_file
+                    ):
+                        config_file = fix_config(
+                            config_file,
+                            interactive,
+                            key,
+                            VALIDITY[key]["valid_text"],
+                            default,
+                        )
+                else:
+                    if key not in config_file or not VALIDITY[key]["func"](
+                        value=config_file[key]
+                    ):
+                        config_file = fix_config(
+                            config_file,
+                            interactive,
+                            key,
+                            VALIDITY[key]["valid_text"],
+                            default,
+                        )
+
 
     return config_file
